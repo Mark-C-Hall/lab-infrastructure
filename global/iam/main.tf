ca8a88894662d88a82f299de6b665500e8bf5e39
Mark Hall <mark.hall993@gmail.com>
2024-06-19 11:56:32 -0400

    Add IAM for github actions

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -12,73 +12,11 @@ terraform {
   }
 }
 
-resource "aws_iam_user" "example" {
-  for_each = toset(var.usernames)
-  name     = each.value
-}
-
-resource "aws_iam_policy" "cloudwatch_read_only" {
-  name   = "cloudwatch_read_only"
-  policy = data.aws_iam_policy_document.cloudwatch_read_only.json
-}
-
-data "aws_iam_policy_document" "cloudwatch_read_only" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "cloudwatch:Describe*",
-      "cloudwatch:Get*",
-      "cloudwatch:List*",
-    ]
-
-    resources = ["*"]
-  }
-}
-
-resource "aws_iam_policy" "cloudwatch_read_and_write" {
-  name   = "cloudwatch_read_and_write"
-  policy = data.aws_iam_policy_document.cloudwatch_read_and_write.json
-}
-
-data "aws_iam_policy_document" "cloudwatch_read_and_write" {
-  statement {
-    effect = "Allow"
-
-    actions = ["cloudwatch:*"]
-
-    resources = ["*"]
-  }
-}
-
-variable "usernames" {
-  description = "A list of usernames to create"
-  type        = list(string)
-  default     = ["neo", "trinity", "morpheus"]
-}
-
-variable "hero_with_1000_faces" {
-  description = "map"
-  type        = map(string)
-  default = {
-    "neo"      = "The One"
-    "trinity"  = "The Beloved"
-    "morpheus" = "The Mentor"
-  }
-}
-
-output "all_usernames" {
-  value = aws_iam_user.example
-}
-
-output "all_arns" {
-  value = values(aws_iam_user.example)[*].arn
-}
-
-output "upper_names" {
-  value = [for name in values(aws_iam_user.example) : upper(name.name)]
-}
-
-output "bios" {
-  value = [for name, bio in var.hero_with_1000_faces : "${name} is the ${bio}"]
+module "iam" {
+  source = "../../../modules/iam"
+  allowed_repos_branches = [{
+    org    = "Mark-C-Hall"
+    repo   = "Portfolio"
+    branch = "main"
+  }]
 }
